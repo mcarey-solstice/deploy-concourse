0d1ea0c82119ff66505f180b65b206bf3d3f95ef
Mike Carey <mcarey@solstice.com>
2018-03-28 14:47:25 -0500

    Move non-configurations out of the env file

---
+++ scripts/load-env.sh
@@ -0,0 +1,41 @@
+#!/bin/bash -e
+
+###
+# Description:
+#   Loads the environment based on the `FOUNDATION` environment variable;
+#     otherwise, loads the env file
+#
+# Usage:
+#   source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"/load-env.sh
+##
+
+declare -r __BASEDIR__="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+
+export BOSH_CMD=bosh
+export VAULT_CMD=vault
+export JQ_CMD=jq
+export CREDHUB_CMD=credhub
+
+if [[ "$FOUNDATION" != "" ]]; then
+  echo "sourcing $__BASEDIR__/scripts/$FOUNDATION-env...."
+  source "$__BASEDIR__"/scripts/$FOUNDATION-env
+else
+  echo "sourcing $__BASEDIR__/env...."
+  source "$__BASEDIR__"/scripts/env
+fi
+
+source "$__BASEDIR__"/releases
+
+UNAME=$(uname)
+if [[ "$UNAME" == "Darwin" && "$CREDENTIAL_MANAGER" == "vault" ]]; then
+  networksetup -setdnsservers Wi-Fi $DNS_SERVERS
+fi
+
+export http_proxy=$HTTP_PROXY
+export https_proxy=$HTTPS_PROXY
+
+if [[ "$HTTP_PROXY_REQUIRED" == "true" ]]; then
+  printf -v NO_PROXY '%s,' $(eval echo $NO_PROXY_PATTERN)
+  export NO_PROXY
+  export no_proxy=$NO_PROXY
+fi
