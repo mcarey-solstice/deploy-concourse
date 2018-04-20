93da7c8e90bf62db32d4be45ef3b1b2bb794619b
Mike Carey <mcarey@solstice.com>
2018-04-20 15:31:42 -0500

    Clean up

--- scripts/vault-login.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-__DIR__=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
-source $__DIR__/load-env.sh
-
-__BASEDIR__=$(dirname $__DIR__)
-export VAULT_TOKEN=$(cat $__BASEDIR__/$BOSH_ALIAS/vault.log | grep 'Initial Root Token' | awk '{print $4}')
-$VAULT_CMD login $VAULT_TOKEN
