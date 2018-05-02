700568e6337aa0105388269291ed7652e367495f
Mike Carey <mcarey@solstice.com>
2018-05-02 10:11:58 -0500

    Split out bosh functions

---
+++ scripts/bosh/bosh-update-cloud-config.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -e
+
+declare -r __DIR__="$( dirname "$( readlink -f "${BASH_SOURCE[0]}" )" )"
+
+source "$__DIR__"/../helpers
+source "$__DIR__"/../load-env
+
+RESOURCE_POOL_OPS_FILE=" "
+RESOURCE_POOL_VARS=" "
+if [ -n "$VCENTER_RESOURCE_POOL" ]; then
+  RESOURCE_POOL_OPS_FILE=" -o $__BASE_DIR__/ops-files/add-vcenter-resource-pool.yml"
+  RESOURCE_POOL_VARS=" -v vcenter_rp=\"$VCENTER_RESOURCE_POOL\""
+fi
+
+log "Updating the bosh cloud config"
+$BOSH_CMD -n \
+  -e "$BOSH_ALIAS" \
+  update-cloud-config "$__BASEDIR__"/cloud-configs/cloud-config.yml \
+    -v az_name="$CONCOURSE_AZ_NAME" \
+    -v nw_name="$CONCOURSE_NW_NAME" \
+    -v vcenter_cluster="$VCENTER_CLUSTER_NAME" \
+    -v network_cidr="$NETWORK_CIDR" \
+    -v network_name="$VCENTER_NETWORK_NAME" \
+    -v network_gateway="$NETWORK_GATEWAY" \
+    -v dns_servers="[$DNS_SERVERS]" \
+    -v reserved_ips="$RESERVED_IPS" \
+    -v static_ips="$CLOUD_CONFIG_STATIC_IPS" \
+    -v vm_disk_type="$VM_DISK_TYPE" \
+    $RESOURCE_POOL_OPS_FILE $RESOURCE_POOL_VARS
+
+# uploads bosh cloud-config
